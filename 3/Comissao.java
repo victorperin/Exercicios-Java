35fbfb86c360572147141d0c072d25237bd1d266
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-12 09:36:18 -0200

    Preparação exercícios 3 e 4

---
+++ 3/Comissao.java
@@ -0,0 +1,10 @@
+/*************************************************************************
+ * 3) Faça um programa para pagamento de comissão de vendedores de peças,
+ *		levando-se em consideração que sua comissão será de 5% do total da venda 
+ *		e que você tem os seguintes dados:
+ * 		- Identificação do vendedor
+ * 		- Código da peça
+ * 		- Preço unitário da peça
+ * 		- Quantidade vendida
+
+*************************************************************************/
