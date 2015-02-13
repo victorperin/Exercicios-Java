b3d66e9eee478a05bf7ef45560b71438258816c1
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-13 10:27:25 -0200

    Atualização diária

---
+++ 5/Exercicio.java
@@ -0,0 +1,8 @@
+/*****************************************************************************************************************
+* 5) Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz
+* 	12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média.
+* 	Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE.
+* 	Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a
+* 	fórmula: LITROS_USADOS = DISTANCIA / 12. O programa deve apresentar os valores da velocidade média,
+* 	tempo gasto, a distância percorrida e a quantidade de litros utilizada na viagem. Dica: trabalhe com valores reais.
+*****************************************************************************************************************/
---
+++ 6/Exercicio.java
@@ -0,0 +1,5 @@
+/*****************************************************************************************************************
+* 6) Ler uma temperatura em graus Celsius e apresentá-Ia convertida em graus Fahrenheit. A fórmula de conversão de
+* 	temperatura a ser utilizada é F = (9 * C + 160) / 5, em que a variável F representa é a temperatura em graus
+* 	Fahrenheit e a variável C representa é a temperatura em graus Celsius.
+*****************************************************************************************************************/
