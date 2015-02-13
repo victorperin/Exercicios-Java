ae1b729b9ed3ba3c4670a093a5a4ff301cd30c7b
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-13 11:01:53 -0200

    Exercício 6

--- 6/Exercicio.java
+++ 6/Exercicio.java
@@ -3,3 +3,26 @@
 * 	temperatura a ser utilizada é F = (9 * C + 160) / 5, em que a variável F representa é a temperatura em graus
 * 	Fahrenheit e a variável C representa é a temperatura em graus Celsius.
 *****************************************************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio{
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		double Celsius, Fahrenheit; //Consegui colocar o nome de variável mais difícil do mundo! o/
+
+		System.out.print("Conversor de temperatura: Graus Celsios -> Fahrenheit\n\n"); //Pronto, bem mais simples!
+		
+		
+		System.out.print("Digite a temperatura em Celsius: ");
+		Celsius = entrada.nextDouble();
+		
+		Fahrenheit = ( 9 * Celsius + 160 ) / 5;
+
+		System.out.print("\n A medida convertida é " + Fahrenheit + "ºF\n");
+		
+	}
+
+}
