65387dd7149962a01feaf0f38e3ceb712cb67f84
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-16 00:41:55 -0200

    Exercício 12

---
+++ 12/Exercicio.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+* 12) Escreva um programa que leia um número inteiro e exiba o seu módulo.
+*   O módulo de um número x é:
+*   x se x é maior ou igual a zero
+*   x * (-1) se x é menor que zero
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int Numero,ModuloNumero;
+
+      	System.out.print("Módulo de Número\n\n"); //Finalmente...
+
+
+      	System.out.print("Digite um número inteiro:  ");
+      	Numero = entrada.nextInt();
+
+
+      	if(Numero >= 0) ModuloNumero = Numero;
+        else ModuloNumero = Numero*(-1);
+
+        System.out.print("O módulo do número é "+ModuloNumero+"\n");
+
+    }
+
+}
