89d9a06c4c15d15d852709967ad9b02389a49c17
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-26 14:57:29 -0300

    Exercício 26

---
+++ 26/Exercicio.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+* 26) Escreva um programa que exiba os números de 1 a 100 na tela em ordem
+* decrescente.
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Exercicio {
+
+  public static void main(String[] args) {
+  
+    Scanner entrada = new Scanner(System.in);
+    for(int x =100;x>0;x--){
+      System.out.println(x);
+    }
+    
+  }
+}
