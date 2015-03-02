c1f4cab37670697f7b622fa11563096745f43624
Victor Perin <victor_perin@nerdhitech.com.br>
2015-03-02 10:25:05 -0300

    Exercício 29

---
+++ 29/Exercicio.java
@@ -0,0 +1,14 @@
+/*******************************************************************************
+* 29) Faça um programa que exiba os números de 1 até 2000.
+*******************************************************************************/
+public class Exercicio {
+
+  public static void main(String[] args) {
+
+    for(int x=1; x <= 2000;x++){
+        System.out.println(x);
+    }
+
+
+  }
+}
