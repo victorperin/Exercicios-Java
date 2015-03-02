72b77c335617d6c5728abb65baebc781dd88c0cf
Victor Perin <victor_perin@nerdhitech.com.br>
2015-03-02 10:24:31 -0300

    Exercício 28

---
+++ 28/Exercicio.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* 28) Faça um programa que gera e escreve os números ímpares dos números lidos
+* entre 100 e 200.
+*******************************************************************************/
+public class Exercicio {
+
+  public static void main(String[] args) {
+
+    for(int x=100; x <= 200;x++){
+      if(x%2==1){
+        System.out.println(x);
+      }
+    }
+
+
+  }
+}
