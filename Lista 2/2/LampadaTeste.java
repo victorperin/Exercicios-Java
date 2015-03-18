432e3a03c84034fca77635e094ada3f0fc8b4fec
Victor Perin <victor_perin@nerdhitech.com.br>
2015-03-18 10:26:51 -0300

    Fim exercício 2 - Lista 2

--- Lista 2/2/LampadaTeste.java
+++ Lista 2/2/LampadaTeste.java
@@ -1,15 +1,25 @@
-/*************************************************************************
-* 2. Implementar uma classe Lampada que deve possuir como características * tipo (led, fluorescente,...), voltagem,
-* cor, marca, preço, potência, status(boolean). Nesta classe devem ser * * implementados os métodos
-* construtores, getters, setters, toString, ascender e apagar. Em uma * * * outra classe chamada LampadaTeste
-* devem ser criados dois objetos do tipo Lampada, atribuir valores e * * * exibir os dados deste objeto. O programa
-* deverá informar também qual das duas lâmpadas possui maior potência e * * também qual das lâmpadas é a
-* mais cara.
-*************************************************************************/
+/*******************************************************************************
+* 2. Implementar uma classe Lampada que deve possuir como características tipo
+* (led, fluorescente,...), voltagem, cor, marca, preço, potência,
+* status(boolean). Nesta classe devem ser implementados os métodos construtores,
+* getters, setters, toString, ascender e apagar. Em uma outra classe chamada
+* LampadaTeste devem ser criados dois objetos do tipo Lampada, atribuir valores
+* e exibir os dados deste objeto. O programa deverá informar também qual das
+* duas lâmpadas possui maior potência e também qual das lâmpadas é a mais cara.
+*******************************************************************************/
 
 import java.util.Scanner;
 
 public class LampadaTeste {
-	
+
+  public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+
+    Lampada lampadaAzul = new Lampada("led",110,"Azul","Baconzitos",10.32, 200,false);
+    Lampada lampadaVerde = new Lampada("led",220,"Verde","Bacon Supreme",11.15, 350,false);
+
+    System.out.print(lampadaAzul.toString());
+    System.out.print(lampadaVerde.toString());
+  }
 
 }
