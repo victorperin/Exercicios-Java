a5093b2abaa5e476f9a2df501b7bad31ff99e336
Victor perin <me@victorperin.com>
2015-03-17 22:03:19 -0300

    Inicio exercicio 2 lista 2

---
+++ Lista 2/2/LampadaTeste.java
@@ -0,0 +1,15 @@
+/*************************************************************************
+* 2. Implementar uma classe Lampada que deve possuir como características * tipo (led, fluorescente,...), voltagem,
+* cor, marca, preço, potência, status(boolean). Nesta classe devem ser * * implementados os métodos
+* construtores, getters, setters, toString, ascender e apagar. Em uma * * * outra classe chamada LampadaTeste
+* devem ser criados dois objetos do tipo Lampada, atribuir valores e * * * exibir os dados deste objeto. O programa
+* deverá informar também qual das duas lâmpadas possui maior potência e * * também qual das lâmpadas é a
+* mais cara.
+*************************************************************************/
+
+import java.util.Scanner;
+
+public class LampadaTeste {
+	
+
+}
