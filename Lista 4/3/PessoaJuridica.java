7047dcd05ad09aa6fbb67fe05f0ed1dba6bf9408
Victor Perin <victor_perin@nerdhitech.com.br>
2015-03-24 09:33:02 -0300

    PessoaJuridica

---
+++ Lista 4/3/PessoaJuridica.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+Exercicio no arquivo de imagem.
+*******************************************************************************/
+import java.util.Scanner;
+
+public class PessoaJuridica extends Cliente{
+	private String cnpj;
+	private String nomeFantasia;
+
+	//construtor
+	public Funcionario(String nome, String endereco,String telefone,String cnpj,String nomeFantasia){
+		super(nome,endereco,telefone);
+		this.cnpj = cnpj;
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	//gets
+	public double getCnpj(){
+		return this.cnpj;
+	}
+
+	//sets
+	public void setNomeFantasia(String nomeFantasia){
+		this.nomeFantasia = nomeFantasia;
+	}
+}
