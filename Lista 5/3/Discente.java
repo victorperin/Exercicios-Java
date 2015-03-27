1d8cc83a496f2d59a0f1adc489b1f589cae2c2a0
Victor Perin <victor_perin@nerdhitech.com.br>
2015-03-27 11:24:36 -0300

    Adicionando classe Discente

---
+++ Lista 5/3/Discente.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+Exercício 3)
+Uma universidade deseja modificar a estrutura de sua base de dados. Para isso, ela quer construir a
+hierarquia das pessoas que frequentam o estabelecimento com a ajuda de uma representação do tipo
+OO (orientado objeto). A universidade deseja conservar o nome de todas as pessoas e dispõe de
+uma função que lhe permite de inicializar e de modificar este membro dado. A universidade é
+frequentada por estudantes e empregados. As fichas dos estudantes devem conter seu nome, seu
+estatuto (aluno especial, tempo parcial, tempo integral) e sua média. Além do mais, é necessário
+prever funções que poderão modificar o estatuto e a média. Os empregados são divididos em duas
+categorias: o corpo docente (demonstrador, monitor, professor) e o discente (escritório e
+manutenção). Para todos os empregados, nós devemos prever um campo salário e uma função que
+permita fixar os salários. Escreva, em linguagem JAVA, as declarações das classes que permitem
+representar esta hierarquia.
+*******************************************************************************/
+
+public class Discente extends Empregado{
+  private String tipo;
+
+  public Discente(String nome,double salario,String tipo){
+    super(nome,salario);
+    setTipo(tipo);
+  }
+
+  //setters
+  public void setTipoDiscente(String tipo){
+    if(tipo=="escritório"||tipo=="manutenção") setTipo(tipo);
+    else System.out.println("Atenção, tipo de docente errado.");
+  }
+}
