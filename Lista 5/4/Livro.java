d387641d7d9491577c31eb8268fef14155f04378
Victor Perin <me@victorperin.com>
2015-05-21 13:50:19 -0300

    Criando o básico do objeto Livro

---
+++ Lista 5/4/Livro.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+Exercício 4)
+Escreva uma classe Livro que represente os dados básicos de um livro.
+• Crie uma classe LivroLivraria que represente os dados básicos de um livro que
+    está venda em uma livraria.
+• Crie uma classe LivroBiblioteca que represente os dados básicos de um livro
+    que pode ser emprestado a leitores.
+• Escreva também um programa que demonstre o uso destas classes.
+*******************************************************************************/
+public class Livro{
+  private String nome,autor,resumo;
+  private Data dataLancamento;
+
+  //construtor
+  public Livro(){
+
+  }
+}
