8741a9ebddc64b139a433d6b28dd7de74b40ca32
Victor Perin <me@victorperin.com>
2015-05-22 15:07:03 -0300

    Inserindo exemplo de output

--- Lista 5/4/Exercicio.java
+++ Lista 5/4/Exercicio.java
@@ -8,7 +8,18 @@ Escreva uma classe Livro que represente os dados básicos de um livro.
 • Escreva também um programa que demonstre o uso destas classes.
 *******************************************************************************/
 public class Exercicio{
-  public static void Main(String[] args){
-    
+  public static void main(String[] args){
+    Livro eurobo = new Livro("Eu, robô", "Isaac Asimov", "02/12/1950");
+    LivroLivraria euroboLivraria = new LivroLivraria(eurobo,45.00f,300);
+    LivroBiblioteca euroboBiblioteca = new LivroBiblioteca(eurobo);
+
+
+    System.out.println("Livro: "+euroboLivraria.getNomeLivro());
+    System.out.println("Preço: "+euroboLivraria.getPreco());
+    System.out.println("Quantidade de livros em estoque: "+euroboLivraria.getQuantidade());
+
+    System.out.println("Livro: "+euroboBiblioteca.getNomeLivro());
+    System.out.println("leitor: "+euroboBiblioteca.getLeitor());
+    System.out.println("Data de devolução: "+euroboBiblioteca.getDataDevolucao());
   }
 }
