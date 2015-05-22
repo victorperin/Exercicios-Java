d4a3a28e59a07b16fc7d64f03eeaa82b998f93a2
Victor Perin <me@victorperin.com>
2015-05-22 11:07:01 -0300

    Inserindo classe Livro Biblioteca

---
+++ Lista 5/4/LivroBiblioteca.java
@@ -0,0 +1,9 @@
+/*******************************************************************************
+Exercício 4)
+Escreva uma classe Livro que represente os dados básicos de um livro.
+• Crie uma classe LivroLivraria que represente os dados básicos de um livro que
+    está venda em uma livraria.
+• Crie uma classe LivroBiblioteca que represente os dados básicos de um livro
+    que pode ser emprestado a leitores.
+• Escreva também um programa que demonstre o uso destas classes.
+*******************************************************************************/
