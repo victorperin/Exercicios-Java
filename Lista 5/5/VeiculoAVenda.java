3d564cdc1917a61187add9f4b862458fe8c4bc90
Victor Perin <me@victorperin.com>
2015-05-26 12:32:12 -0300

    iniciando VeiculoAVenda

---
+++ Lista 5/5/VeiculoAVenda.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+Exercício 5)
+Escreva uma classe VeiculoAVenda que represente informações básicas sobre um veículo genérico que esteja à venda, como tipo (podendo ser representado por uma String), ano e precoDeVenda.
+• Escreva uma classe AutomovelAVenda que herde de VeiculoAVenda e que inicialize o campo tipo com o valor constante “Automóvel”,
+• Escreva uma classe MotocicletaAVenda que herde de VeiculoAVenda e que inicialize o campo tipo com o valor “Motocicleta”.
+• Escreva também um programa que demonstre o uso destas classes usando Polimorfismo.
+*******************************************************************************/
+public class VeiculoAVenda{
+  private String tipo;
+  private int ano;
+  private float precoDeVenda;
+
+  public VeiculoAVenda(String tipo, int ano, float precoDeVenda){
+    this.tipo = tipo;
+    this.ano = ano;
+    this.precoDeVenda = precoDeVenda;
+  }
+
+  //getters
+  public String getTipo(){
+    return this.tipo;
+  }
+  public int getAno(){
+    return this.ano;
+  }
+  public float getPrecoDeVenda(){
+    return this.precoDeVenda;
+  }
+}
