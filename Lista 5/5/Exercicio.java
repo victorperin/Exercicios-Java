4f286d678925af26aeb034373ab66b1eb1bd622b
Victor Perin <me@victorperin.com>
2015-05-26 14:25:01 -0300

    Finalizando a demonstração...

--- Lista 5/5/Exercicio.java
+++ Lista 5/5/Exercicio.java
@@ -5,12 +5,19 @@ Escreva uma classe VeiculoAVenda que represente informações básicas sobre um 
 • Escreva uma classe MotocicletaAVenda que herde de VeiculoAVenda e que inicialize o campo tipo com o valor "Motocicleta".
 • Escreva também um programa que demonstre o uso destas classes usando Polimorfismo.
 *******************************************************************************/
+import java.util.ArrayList;
+
 public class Exercicio{
   public static void main(String[]args){
 
-    VeiculoAVenda veiculo = new AutomovelAVenda("fusca",1970,10000.00f);
-    VeiculoAVenda moto = new MotocicletaAVenda("fusca",1970,10000.00f);
-    System.out.println(veiculo.getTipo());
-    System.out.println(moto.getTipo());
+    ArrayList<VeiculoAVenda> veiculos = new ArrayList<VeiculoAVenda>();
+    veiculos.add(new AutomovelAVenda("fusca",1970,10000.00f));
+    veiculos.add(new MotocicletaAVenda("Alguma moto conhecida, porque eu não conheço nenhuma...",2012,7023.05f));
+
+    for(int x=0; x<veiculos.size();x++){
+      System.out.println(veiculos.get(x).toString());
+    }
+
+
   }
 }
